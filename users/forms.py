86666fdbe189fd0c2099f3f66a9e0d4d6a6d5c39
Mario Krajacic <krajacic.mario@gmail.com>
2018-10-18 15:52:38 +0200

    Building

--- users/forms.py
+++ users/forms.py
@@ -1,6 +1,7 @@
 from django import forms
 from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserCreationForm
+from .models import Profile
 
 class UserRegisterForm(UserCreationForm):   # UserRegistarionForm nasljedjuje UserCreationForm
     email = forms.EmailField()              # dodajem polje koje zelim, naziv = tip polja i ovdje mogu stavljati arbumente default itd
@@ -8,3 +9,17 @@ class UserRegisterForm(UserCreationForm):   # UserRegistarionForm nasljedjuje Us
     class Meta:
         model = User    # ovime formi govorim s kojim modelom da komunicira
         fields = ['username', 'email', 'password1', 'password2']    # polja koja ce biti prikazana na formi i njihov redoslijed
+
+
+class UserUpdateForm(forms.ModelForm):
+    email = forms.EmailField()
+
+    class Meta:
+        model = User
+        fields = ['username', 'email']
+
+
+class ProfileUpdateForm(forms.ModelForm):
+    class Meta:
+        model = Profile
+        fields = ['image']
